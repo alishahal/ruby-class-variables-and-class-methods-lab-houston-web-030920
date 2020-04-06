965e86924e75b60abd2cb60c83285d073258fe5b
Karim Macknojia <alishahal@msn.com>
2020-04-06 19:58:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
    def initialize(name, artist, genre)
     @name = name
     @artist = artist
-    #@genre = genre
+    @genre = genre
     @@count +=1
     @@genres << genre
     @@artists << artist
