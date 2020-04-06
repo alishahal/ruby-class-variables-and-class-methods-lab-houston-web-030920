9b5e8634fed3c65cfe856a1fd71d27c15a896515
Karim Macknojia <alishahal@msn.com>
2020-04-06 19:59:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,11 +21,11 @@ class Song
   end
   
   def self.genres
-    @@genres.unique
+    @@genres.unique!
   end
   
   def self.artists
-    @@artists.unique
+    @@artists.unique!
   end
   
   def self.genre_count
